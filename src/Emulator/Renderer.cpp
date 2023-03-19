514368d2a308b12600f3b52c3a7c9e65d8dcac72
Muzuwi <kedziora99@gmail.com>
2023-03-19 12:34:02 +0100

    Extern: Bump imgui to v1.89.4

--- src/Emulator/Renderer.cpp
+++ src/Emulator/Renderer.cpp
@@ -3,7 +3,7 @@
 #include <fmt/format.h>
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
-#include <imgui_impl_sdl.h>
+#include <imgui_impl_sdl2.h>
 #include <thread>
 #include "APU/APU.hpp"
 #include "Bus/IO/Keypad.hpp"
